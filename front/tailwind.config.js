c4ab369adccc88fca14a9027e03389ba723b8e25
Aziz <takaliaziz4@gmail.com>
2022-09-06 15:25:24 +0100

    tailwind off

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} 
 module.exports = {
     content: ["./src/components/Dashboard/Example.jsx"],
     theme: {
         extend: {},
     },
     plugins: [],
-}
+}*/
