7ad7e07206f6f9975b90eaef5c8b14391b0d4508
Aziz <takaliaziz4@gmail.com>
2022-09-12 22:28:51 +0100

    config tailwind css

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    content: ["./src/components/Dashboard/Example.jsx"],
-    purge: [],
+    purge: ["./src/components/Dashboard/Example.jsx"],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
